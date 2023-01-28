f2d6082c6df7558a65c7afd9d33f97fe075599de
David <developerdavid00@gmail.com>
2023-01-28 15:35:55 -0300

    Criada rota para exibir dados na view.

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CreatePedido;
 use App\Http\Controllers\RegisterPedidoController;
-
+use App\Http\Controllers\ShowPedidosController;
 // Rotas
 Route::get('/', [HomeController::class, 'Home']);
 Route::get('/createPedido', [CreatePedido::class, 'CreatePedido']);
 Route::post('/registerPedido', [RegisterPedidoController::class, 'RegisterPedido']);
+Route::get('/showPedidos', [ShowPedidosController::class, 'ShowPedidos']);
