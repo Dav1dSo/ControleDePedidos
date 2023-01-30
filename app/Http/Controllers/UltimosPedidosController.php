572f1b7c7d4393ade70700197ebd102787bf4f2a
David <developerdavid00@gmail.com>
2023-01-30 19:19:22 -0300

    Adionado método pluck.

--- app/Http/Controllers/UltimosPedidosController.php
+++ app/Http/Controllers/UltimosPedidosController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use \Carbon\Carbon;
+use Illuminate\Support\Arr;
 
 class UltimosPedidosController extends Controller
 {
@@ -16,13 +17,18 @@ class UltimosPedidosController extends Controller
         // 6 meses atras
         $dataInicio = Carbon::now()->sub('6 months');
 
+        // $ultimosMeses = DB::table('pedidos')
+        // ->selectRaw(
+        //     'created_at, nome, telefone, rua, cidade, estado, numero, bairro, valor, data_pedido, status, observacao '
+        // )->whereBetween('data_pedido', [$dataInicio, $datenow] )->get();
+
         $ultimosMeses = DB::table('pedidos')
-        ->selectRaw(
-            'created_at, nome, telefone, rua, cidade, estado, numero, bairro, valor, data_pedido, status, observacao '
-        )->whereBetween('data_pedido', [$dataInicio, $datenow] )->get();
+        ->selectRaw(' nome ')->whereBetween('data_pedido', [$dataInicio, $datenow] )->get();
 
+        $teste = $ultimosMeses;
 
+        $nome = $teste->pluck('nome');
         
-        return view('UltimosPedidos', ['ultimosMeses' => $ultimosMeses]);
+        return view('UltimosPedidos', ['ultimosMeses' => $ultimosMeses, 'nome' => $nome]);
     }
 }  
