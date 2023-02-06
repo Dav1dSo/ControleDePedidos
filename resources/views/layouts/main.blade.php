7e97c0ec43000b0004bd6f23fd2fc07d84ab5c0b
David <developerdavid00@gmail.com>
2023-02-05 22:20:58 -0300

    Correção semântica.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -117,10 +117,10 @@
     @yield('createpedido')
     @yield('showPedidos')
     @yield('titleUltimosPedidos')
+    @yield('count')
     @yield('select6meses')
     @yield('PedidosUes')
     @yield('PedidosAbertos')
-    @yield('count')
 
 </body>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
